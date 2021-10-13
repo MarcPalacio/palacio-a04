f85998e80f369271750b4fb2a1357741b4f52377
MarcPalacio <mpalacio2020@gmail.com>
2021-10-12 23:41:13 -0400

    added exercise41 pseudocode and UML

---
+++ exercise41/src/main/java/baseline/NameSorter.java
@@ -0,0 +1,38 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Marc Palacio
+ */
+
+package baseline;
+
+import java.io.*;
+import java.util.*;
+
+public class NameSorter {
+    private ArrayList<String> listNames;
+
+    public void scanInputFile(File inputFileName){
+        // Creates file reader
+        // while file reader has nextLine
+        //      Uses reader to read line and add to list
+    }
+
+    public String createOutput(){
+        // Creates StringBuilder
+        // Adds header of list output to output
+        // for length of listNames
+        //      add string to output
+        // Returns string
+        return "";
+    }
+
+    public void writeToFile(String write){
+        // Creates an output file
+        // Creates file writer
+        // Writes string from createOutput to file
+    }
+
+    private void sortList(){
+        // Sorts listNames
+    }
+}
---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Marc Palacio
+ */
+
+package baseline;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution41 {
+    public static void main(String[] args){
+        // Creates instance of NameSorter
+        // Creates file object for input
+        // Calls scanInput from NameSorter
+        // Calls sortList from NameSorter
+        // Calls createOutput from NameSorter
+        // Calls writeToFile from NameSorter
+    }
+}
